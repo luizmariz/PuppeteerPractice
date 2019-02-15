aba0a80b1950ddf431ef6551b87948c3ae95e5e9
Luiz <37978726+luizmariz@users.noreply.github.com>
2019-02-15 00:08:14 -0200

    *autoscroll

--- app.js
+++ app.js
@@ -2,16 +2,36 @@ const puppeteer = require('puppeteer');
 
 const URL = 'https://medium.com/search?q=angular';
 
+async function autoScroll(page){
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            const totalHeight = 0;
+            const distance = 100;
+            const timer = setInterval(() => {
+                const scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if(totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 600);
+        });
+    });
+};
+
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto( URL );
 
-    while(true) {
-      await page.evaluate( () => {
-        window.scrollBy(0, window.innerHeight);
-      });
-    }
+  await autoScroll(page);
+
+  await page.screenshot({
+      path: 'yoursite.png',
+      fullPage: true
+  });
 
   await browser.close();
 })();
