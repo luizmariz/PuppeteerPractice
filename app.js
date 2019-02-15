382ac174d2b37f421b3c334cadd24e5c76db25a1
Luiz Gustavo Oliveira Mariz <luiz.mariz@MJV.net>
2019-02-15 18:46:34 -0200

    +base scrap

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
 const puppeteer = require('puppeteer');
 
-const URL = 'https://medium.com/search?q=angular';
+const URL = 'https://medium.com/search?q=node';
 
-async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            const totalHeight = 0;
-            const distance = 100;
-            const timer = setInterval(() => {
-                const scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
+const ARTICLE_ITEM_CLASS = 'postArticle postArticle--short js-postArticle js-trackPostPresentation';
 
-                if(totalHeight >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 600);
-        });
-    });
+const autoScrollAndScrap = async page => {
+	await page.evaluate(async ARTICLE_ITEM_CLASS => {
+		await new Promise((resolve, reject) => {
+			const distance = 2000;
+			let index = 0;
+			this.erro = 0;
+			const timer = setInterval(() => {
+				try {
+					console.log(index)
+					console.log(document.getElementsByClassName(ARTICLE_ITEM_CLASS).length)
+					const listItem = ({ 
+						url: document.getElementsByClassName(ARTICLE_ITEM_CLASS)[index].childNodes[1].firstElementChild.href,
+						title: document.getElementsByClassName(ARTICLE_ITEM_CLASS)[index].getElementsByClassName('postArticle-content')[0].firstElementChild.firstElementChild.lastElementChild.firstElementChild.getElementsByClassName('graf--title')[0] || document.getElementsByClassName(ARTICLE_ITEM_CLASS)[index].getElementsByClassName('postArticle-content')[0].firstElementChild.firstElementChild.lastElementChild.firstElementChild.getElementsByClassName('graf--trailing') || { textContent: "" },
+						subtitle: document.getElementsByClassName(ARTICLE_ITEM_CLASS)[index].getElementsByClassName('postArticle-content')[0].firstElementChild.firstElementChild.lastElementChild.firstElementChild.getElementsByClassName('graf--trailing')[0] || { textContent: "" },
+					});
+
+					console.log(listItem.url,"\n",listItem.title.textContent,"\n",listItem.subtitle.textContent);
+
+					window.scrollBy(0, distance);
+					index++;
+
+				} catch (error){
+						this.erro++;
+						if ( erro >= 10) {
+							clearInterval(timer);
+							resolve();
+						}
+
+				}
+			}, 400);
+		});
+	}, ARTICLE_ITEM_CLASS);
 };
 
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto( URL );
-
-  await autoScroll(page);
-
-  await page.screenshot({
-      path: 'yoursite.png',
-      fullPage: true
-  });
-
+	await page.goto( URL );
+  await autoScrollAndScrap(page);
   await browser.close();
 })();
